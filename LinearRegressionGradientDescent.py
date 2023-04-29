b521618dae9efbe3f76262117e741a66e47b907b
Sivtheng <sivtheng25@gmail.com>
2023-04-29 14:14:40 +0700

    first

---
+++ LinearRegressionGradientDescent.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# Preprocessing Input data
+data = pd.read_csv('Salary_dataset.csv')
+X = data.iloc[:, 0]
+Y = data.iloc[:, 1]
+
+# Building the model
+m = 0
+c = 0
+
+L = 0.0001  # The learning Rate
+epochs = 1000  # The number of iterations to perform gradient descent
+
+n = float(len(X)) # Number of elements in X
+
+# Performing Gradient Descent 
+for i in range(epochs): 
+    Y_pred = m*X + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c
+    
+print (m, c)
+#lll
+# Making predictions
+Y_pred = m*X + c
+
+# Calculate RMSE and MAE using sklearn
+rmse = mean_squared_error(Y, Y_pred, squared=False)
+mae = mean_absolute_error(Y, Y_pred)
+print("RMSE:", rmse)
+print("MAE:", mae)
+
+plt.scatter(X, Y) 
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
+plt.show()
+
